6ef5df1f2e73ab87e3f1f3e71b99c9a924661393
Mohammad72021 <cr.7mr.a@gmail.com>
2023-07-02 10:32:30 +0330

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TesthomeComponent } from './testmodule/testhome/testhome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"test",component:TesthomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
